0930cfa2dde28aadc72a590416e9ca9539b1639f
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-28 15:25:05 +0530

    check otp  working

--- src/components/otpwithapi.js
+++ src/components/otpwithapi.js
@@ -5,6 +5,7 @@ const GetOtpHandler = () => {
   const [name, setName] = useState("");
   const [submitOtp, setSubmitOtp] = useState("");
   const [otp, setOpt] = useState("");
+  const [output, setOutput] = useState(null);
 
   let url = "https://otpgenerator.ishanjirety.repl.co/get-otp?name=";
 
@@ -14,15 +15,6 @@ const GetOtpHandler = () => {
       // console.log(response);
     });
     // console.log({ name });
-  };
-
-  const checkOtp = () => {
-    console.log({ submitOtp });
-    if (name == submitOtp) {
-      alert("correct");
-    } else {
-      alert("wrong");
-    }
   };
 
   return (
@@ -47,7 +39,16 @@ const GetOtpHandler = () => {
           setSubmitOtp(e.target.value);
         }}
       />
-      <button onClick={checkOtp}>CHECK</button>
+      <button
+        onClick={() => {
+          otp === submitOtp
+            ? setOutput(<h1>Correct OTP</h1>)
+            : setOutput("Wrong OTP");
+        }}
+      >
+        CHECK
+      </button>
+      {output}
     </div>
   );
 };
